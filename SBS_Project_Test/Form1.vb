22ecd7883665e6caa8168fff54b2a4a53c2b2cc1
TerryGeng <gengyanda@gmail.com>
2013-07-13 14:16:45 +0800

    Added a function to make log textbox resize with window.

--- SBS_Project_Test/Form1.vb
+++ SBS_Project_Test/Form1.vb
@@ -1,5 +1,8 @@
 ﻿Public Class Form1
 
+    Dim origin_window_height As Integer
+    Dim origin_textbox_height As Integer
+
     Dim mPraser As GrammarPraser
     Dim sentenceList As New ArrayList()
 
@@ -14,6 +17,17 @@
 
     Sub Debug_Output(ByVal msg As String)
         DebugText.AppendText(msg + vbCrLf)
+    End Sub
+
+    Private Sub Form1_Resize(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Resize
+        If origin_window_height <> 0 Then
+            DebugText.Height = Me.Height - origin_window_height + origin_textbox_height
+        End If
+    End Sub
+
+    Private Sub Form1_Activated(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Activated
+        origin_textbox_height = DebugText.Height
+        origin_window_height = Me.Height
     End Sub
 End Class
 
