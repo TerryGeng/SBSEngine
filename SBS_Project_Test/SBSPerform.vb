957fca2b9315399479180bceee879ba2835dff8b
TerryGeng <gengyanda@gmail.com>
2013-07-23 12:04:27 +0800

    Finished the general calculation function

--- SBS_Project_Test/SBSPerform.vb
+++ SBS_Project_Test/SBSPerform.vb
@@ -18,24 +18,30 @@
 
     Dim Performers As StatmentPerformers
     Dim RuntimeData As RuntimeData
+    Dim StdIO As StandardIO
 
-
-    Sub New(ByRef statList As ArrayList)
-        Debug_Output("SBS Perform - Version " + Version + " - Time " + CStr(GetTickCount()))
-        Debug_Output("-----------------")
+    Sub New(ByRef statList As ArrayList, ByRef _stdIO As StandardIO)
+        If _stdIO IsNot Nothing Then
+            StdIO = _stdIO
+            StdIO.PrintLine("SBS Perform - Version " + Version + " - Time " + CStr(GetTickCount()))
+            StdIO.PrintLine("-----------------")
+        Else
+            Throw New ApplicationException("Internal Error: Invalid argument.")
+        End If
 
         If statList IsNot Nothing And statList(0).GetType().Equals(GetType(CodeSequence)) Then
             RuntimeData = New RuntimeData(statList)
+            StdIO = _stdIO
             Performers = New StatmentPerformers()
-            Debug_Output("Statments loaded.")
+            StdIO.PrintLine("Statments loaded.")
         Else
-            Debug_Error("Load Error: Invalid argument type.")
+            Throw New ApplicationException("Internal Error: Invalid argument.")
         End If
     End Sub
 
     Public Function Run() As Integer
         Dim start_time As Long = GetTickCount()
-        Debug_Output("Running start at " + CStr(start_time) + ".")
+        StdIO.PrintLine("Running start at " + CStr(start_time) + ".")
 
         For i As Integer = 0 To RuntimeData.Statments.Count - 1
             Dim statmBody As CodeSequence = RuntimeData.Statments(i).SeqsList(0)
@@ -45,26 +51,18 @@
             If statmType = "EXPRESSION" Then
                 result = Performers.Expression.Perform(statmBody, RuntimeData)
             Else
-                Debug_Error("Internal Error: Unknowed statment type '" + statmType + "'.")
+                Throw New ApplicationException("Internal Error: Unknowed statment type '" + statmType + "'.")
                 Return Nothing
             End If
 
-            Debug_Output("[" + CStr(i) + "]Return Val: (Type:" + result.Type + ")" + CStr(result.Value))
+            StdIO.PrintLine("[" + CStr(i) + "]Return Val: (Type:" + result.Type + ")" + CStr(result.Value))
         Next
 
-        Debug_Output("Running end at " + CStr(GetTickCount()) + ". Total cost " + CStr(GetTickCount() - start_time) + "ms.")
+        StdIO.PrintLine("Running end at " + CStr(GetTickCount()) + ". Total cost " + CStr(GetTickCount() - start_time) + "ms.")
 
         Return 0
     End Function
 
-
-    Sub Debug_Output(ByVal msg As String, Optional ByVal able_not_show As Boolean = False)
-        Form1.DebugText.AppendText(msg + vbCrLf)
-    End Sub
-
-    Sub Debug_Error(ByVal msg As String)
-        Debug_Output(msg + vbCrLf)
-    End Sub
 End Class
 
 
@@ -79,11 +77,24 @@ End Class
 
 Public Class ExpressionPerformer
     Class Calculator
-        Dim Value As SBSValue
+        Dim Value As SBSValue = Nothing
+
+        Dim buffer As Double = 0
+        Dim buffer_available As Boolean = False
 
         Public Sub Calculate(ByVal optr As String, ByVal _value As SBSValue)
+            If Value Is Nothing Then
+                Value = New SBSValue(_value.Type)
+            End If
+
             If optr = "+" Then
                 CalcuAdd(_value)
+            ElseIf optr = "-" Then
+                CalcuSub(_value)
+            ElseIf optr = "*" Then
+                CalcuMulti(_value)
+            ElseIf optr = "/" Then
+                CalcuDiv(_value)
             End If
         End Sub
 
@@ -91,24 +102,45 @@ Public Class ExpressionPerformer
             If Value.Type = "STRING" Then
                 Value.sValue += _value.sValue
             ElseIf Value.Type = "NUMBER" Then
-                Value.nValue += _value.nValue
+                Value.nValue += buffer
+                buffer = _value.nValue
             Else
-                Debug_Error("Runtime Error: Used undefine operator '+' between '" + Value.Type + "' and '" + _value.Type + "'.")
+                Throw New ApplicationException("Runtime Error: Used undefine operation '+' between '" + Value.Type + "' and '" + _value.Type + "'.")
+            End If
+        End Sub
+
+        Public Sub CalcuSub(ByVal _value As SBSValue)
+            If Value.Type = "NUMBER" Then
+                Value.nValue += buffer
+                buffer = -(_value.nValue)
+            Else
+                Throw New ApplicationException("Runtime Error: Used undefine operation '+' between '" + Value.Type + "' and '" + _value.Type + "'.")
             End If
         End Sub
 
         Public Sub CalcuMulti(ByVal _value As SBSValue)
-            Static buffer As Double
-
             If Value.Type = "NUMBER" Then
-
+                buffer *= _value.nValue
+            Else
+                Throw New ApplicationException("Runtime Error: Used undefine operation '*' between '" + Value.Type + "' and '" + _value.Type + "'.")
             End If
+        End Sub
 
+        Public Sub CalcuDiv(ByVal _value As SBSValue)
+            If Value.Type = "NUMBER" Then
+                buffer /= _value.nValue
+            Else
+                Throw New ApplicationException("Runtime Error: Used undefine operation '*' between '" + Value.Type + "' and '" + _value.Type + "'.")
+            End If
         End Sub
 
         Public Function GetResult() As SBSValue
-
+            If buffer <> 0 Then
+                Value.nValue += buffer
+            End If
+            Return Value
         End Function
+
     End Class
 
     Function Perform(ByRef statment As CodeSequence, ByRef runtimeData As RuntimeData) As SBSValue
@@ -121,12 +153,14 @@ Public Class ExpressionPerformer
 
         If firstElement.RuleName = "EXP_ELEMENT" Then
             Dim expElement As CodeSequence = firstElement.SeqsList(0)
-            Dim eleValue As New SBSValue(expElement.RuleName, expElement.Value)
+            Dim eleValue As SBSValue
 
-            If eleValue.Value = "(" Then
+            If expElement.Value = "(" Then
                 Dim bracExpr As CodeSequence = firstElement.SeqsList(1)
                 Dim result As SBSValue = Perform(bracExpr, runtimeData)
-                eleValue = result.Value
+                eleValue = result
+            Else
+                eleValue = New SBSValue(expElement.RuleName, expElement.Value)
             End If
 
             If elementList.Count > 1 Then
@@ -143,14 +177,15 @@ Public Class ExpressionPerformer
         For i As Integer = 0 To opEleList.Count - 1
             Dim curOp As String = opEleList(i).SeqsList(0).SeqsList(0).Value ' EXP_OP_ELEMENT List -> EXP_OP_ELEMENT -> EXP_OP -> "+","-"...
             Dim curEle As CodeSequence = opEleList(i).SeqsList(1).SeqsList(0) ' EXP_OP_ELEMENT List -> EXP_OP_ELEMENT -> EXP_ELEMENT
-            Dim curValue As New SBSValue(curEle.RuleName, curEle.Value)
+            Dim curValue As SBSValue
 
-            If curValue.Value = "(" Then
+            If curEle.Value = "(" Then
                 curEle = opEleList(i).SeqsList(1).SeqsList(1)
                 Dim result As SBSValue = Perform(curEle, runtimeData)
 
-                curValue.Type = result.Type
-                curValue = result.Value
+                curValue = result
+            Else
+                curValue = New SBSValue(curEle.RuleName, curEle.Value)
             End If
 
             calcu.Calculate(curOp, curValue)
@@ -190,11 +225,21 @@ End Class
 
 Public Class SBSValue
     Public Type As String = ""
-    Public nValue As Double
+    Public nValue As Double = 0
     Public sValue As String = ""
-    Sub New(ByVal _type As String, ByVal _value As String)
+
+    Sub New(ByVal _type As String)
         Type = _type
-        sValue = _value
+    End Sub
+
+    Sub New(ByVal _type As String, ByVal _value As String)
+        If _type = "NUMBER" Then
+            Type = _type
+            nValue = CDbl(_value)
+        Else
+            Type = _type
+            sValue = _value
+        End If
     End Sub
     Sub New(ByVal _type As String, ByVal _value As Double)
         Type = _type
