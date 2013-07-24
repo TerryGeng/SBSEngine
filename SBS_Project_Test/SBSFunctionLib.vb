d096a46c3c95127f1abb428c9bdc4e7fff710a3b
TerryGeng <gengyanda@gmail.com>
2013-07-24 21:27:13 +0800

    [AF] Added the system function library.

---
+++ SBS_Project_Test/SBSFunctionLib.vb
@@ -0,0 +1,19 @@
+﻿Public Class SBSFunctionLib
+    Public Shared Sub LoadFunctions(ByRef funcList As ArrayList)
+        funcList.Add(New LibFunction("print", AddressOf Print))
+    End Sub
+
+    Public Shared Function Print(ByRef argsList As ArrayList) As JumpStatus
+        If argsList.Count = 1 Then
+            StandardIO.Print(CStr(argsList(0).Value))
+        Else
+            Error_ArgsAmountNotMatch("Print")
+        End If
+
+        Return Nothing
+    End Function
+
+    Shared Function Error_ArgsAmountNotMatch(ByVal funcName As String)
+        Throw New ApplicationException("Runtime Error: Arguments' amount for '" + funcName + "' doesn't match.")
+    End Function
+End Class
