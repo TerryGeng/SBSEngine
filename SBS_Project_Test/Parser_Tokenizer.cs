d3da504278e40f02f972cf089f93f74e9e871f17
TerryGeng <gengyanda@gmail.com>
2013-11-23 13:59:44 +0800

    [UP] Updated some file.

--- SBS_Project_Test/Parser_Tokenizer.cs
+++ SBS_Project_Test/Parser_Tokenizer.cs
@@ -1,219 +1,268 @@
-﻿Imports System.IO
-Imports System.Text
-Imports LexiconRules = System.Collections.Generic.Dictionary(Of SBSEngine.TokenizerType.LexiconType, SBSEngine.TokenizerType.LexiconRule)
 
-Public Module TokenizerType
-    Public Enum LexiconType
-        Undefined
-        LInteger
-        LFloat
-        LName
-        LString
-        LBlank
-        LCrLf
-        LSymbol
-    End Enum
+using Microsoft.VisualBasic;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using LexiconRules = System.Collections.Generic.Dictionary<SBSEngine.TokenizerType.LexiconType, SBSEngine.TokenizerType.LexiconRule>;
 
-    Public Structure Token
-        Public Type As LexiconType
-        Public Value As String
-    End Structure
+public static class TokenizerType
+{
+	public enum LexiconType
+	{
+		Undefined,
+		LInteger,
+		LFloat,
+		LName,
+		LString,
+		LBlank,
+		LCrLf,
+		LSymbol
+	}
 
-    Public Class LexiconRule
-        Delegate Function ScannerFunction(ByVal Character As Char, ByVal Position As Integer) As ScannerStatus
-        Public Type As LexiconType
-        Public Scan As ScannerFunction
+	public struct Token
+	{
+		public LexiconType Type;
+		public string Value;
+	}
 
-        Sub New(ByVal Type As LexiconType, ByVal Scanner As ScannerFunction)
-            Me.Type = Type
-            Me.Scan = Scanner
-        End Sub
-    End Class
+	public class LexiconRule
+	{
+		public delegate ScannerStatus ScannerFunction(char Character, int Position);
+		public LexiconType Type;
 
-    Enum ScannerStatus
-        ''' <summary>
-        ''' Current and previous characters still match the rule. Match process continue.
-        ''' </summary>
-        ''' <remarks></remarks>
-        Continued
-        ''' <summary>
-        ''' The emergence of current char indicated this combination unmatched the rule.
-        ''' </summary>
-        ''' <remarks></remarks>
-        Unmatch
-        ''' <summary>
-        ''' Current and previous characters matchs the rule. Match process end.
-        ''' </summary>
-        ''' <remarks></remarks>
-        Finished
-        ''' <summary>
-        ''' Only previous characters match the rule. Match process end.
-        ''' </summary>
-        ''' <remarks></remarks>
-        PreviousFinished
+		public ScannerFunction Scan;
+		public LexiconRule(LexiconType Type, ScannerFunction Scanner)
+		{
+			this.Type = Type;
+			this.Scan = Scanner;
+		}
+	}
 
-    End Enum
+	public enum ScannerStatus
+	{
+		/// <summary>
+		/// Current and previous characters still match the rule. Match process continue.
+		/// </summary>
+		/// <remarks></remarks>
+		Continued,
+		/// <summary>
+		/// The emergence of current char indicated this combination unmatched the rule.
+		/// </summary>
+		/// <remarks></remarks>
+		Unmatch,
+		/// <summary>
+		/// Current and previous characters matchs the rule. Match process end.
+		/// </summary>
+		/// <remarks></remarks>
+		Finished,
+		/// <summary>
+		/// Only previous characters match the rule. Match process end.
+		/// </summary>
+		/// <remarks></remarks>
+		PreviousFinished
 
-    Public Class UnexpectedCharacterException
-        Inherits ApplicationException
+	}
 
-        Public Position As Integer
-        Public Character As Char
+	public class UnexpectedCharacterException : ApplicationException
+	{
 
-        Sub New(ByVal Position As Integer, ByVal Character As Char)
-            MyBase.New(String.Format("Unexpected Character '{0}'({1}) at {2}.", Character, Convert.ToInt32(Character), Position))
-            Me.Position = Position
-            Me.Character = Character
-        End Sub
-    End Class
-End Module
+		public int Position;
 
-Module TokenizerRules
-    Sub LoadLexicalRules(ByVal Container As LexiconRules)
-        Container.Add(LexiconType.LInteger, New LexiconRule(LexiconType.LInteger, AddressOf IntegerScanner))
-        Container.Add(LexiconType.LBlank, New LexiconRule(LexiconType.LBlank, AddressOf BlankScanner))
-        Container.Add(LexiconType.LName, New LexiconRule(LexiconType.LName, AddressOf NameScanner))
-        Container.Add(LexiconType.LCrLf, New LexiconRule(LexiconType.LCrLf, AddressOf CrLfScanner))
+		public char Character;
+		public UnexpectedCharacterException(int Position, char Character) : base(string.Format("Unexpected Character '{0}'({1}) at {2}.", Character, Convert.ToInt32(Character), Position))
+		{
+			this.Position = Position;
+			this.Character = Character;
+		}
+	}
+}
 
-    End Sub
+static class TokenizerRules
+{
+	public static void LoadLexicalRules(LexiconRules Container)
+	{
+		Container.Add(LexiconType.LInteger, new LexiconRule(LexiconType.LInteger, IntegerScanner));
+		Container.Add(LexiconType.LBlank, new LexiconRule(LexiconType.LBlank, BlankScanner));
+		Container.Add(LexiconType.LName, new LexiconRule(LexiconType.LName, NameScanner));
+		Container.Add(LexiconType.LCrLf, new LexiconRule(LexiconType.LCrLf, CrLfScanner));
 
-    Function IntegerScanner(ByVal Character As Char, ByVal Position As Integer) As ScannerStatus
-        If Char.IsDigit(Character) Then Return ScannerStatus.Continued
-        Return ScannerStatus.PreviousFinished
-    End Function
+	}
 
-    Function BlankScanner(ByVal Character As Char, ByVal Position As Integer) As ScannerStatus
-        ' Character is neither vbCr nor vbLf
-        If (Not Char.IsControl(Character)) AndAlso _
-            Char.IsWhiteSpace(Character) Then _
-            Return ScannerStatus.Continued
-        Return ScannerStatus.PreviousFinished
-    End Function
+	public static ScannerStatus IntegerScanner(char Character, int Position)
+	{
+		if (char.IsDigit(Character))
+			return ScannerStatus.Continued;
+		return ScannerStatus.PreviousFinished;
+	}
 
-    Function NameScanner(ByVal Character As Char, ByVal Position As Integer) As ScannerStatus
-        ' Variable name cannot start with digit. Otherwise it will be read as two parts.
-        If (Char.IsLetterOrDigit(Character)) OrElse _
-            (Character = "_"c) Then _
-            Return ScannerStatus.Continued
+	public static ScannerStatus BlankScanner(char Character, int Position)
+	{
+		// Character is neither vbCr nor vbLf
+		if ((!char.IsControl(Character)) && char.IsWhiteSpace(Character))
+			return ScannerStatus.Continued;
+		return ScannerStatus.PreviousFinished;
+	}
 
-        Return ScannerStatus.PreviousFinished
-    End Function
+	public static ScannerStatus NameScanner(char Character, int Position)
+	{
+		// Variable name cannot start with digit. Otherwise it will be read as two parts.
+		if ((char.IsLetterOrDigit(Character)) || (Character == '_'))
+			return ScannerStatus.Continued;
 
-    Function CrLfScanner(ByVal Character As Char, ByVal Position As Integer) As ScannerStatus
-        Select Case Position
-            Case 0
-                If Character = vbCr Then Return ScannerStatus.Continued
-            Case 1
-                If Character = vbLf Then Return ScannerStatus.Finished
-        End Select
-        Return ScannerStatus.Unmatch
-    End Function
-End Module
+		return ScannerStatus.PreviousFinished;
+	}
 
-Public Class Tokenizer
-    Dim Reader As SourceCodeReader
-    Dim RulesContainer As LexiconRules
+	public static ScannerStatus CrLfScanner(char Character, int Position)
+	{
+		switch (Position) {
+			case 0:
+				if (Character == Constants.vbCr)
+					return ScannerStatus.Continued;
+				break;
+			case 1:
+				if (Character == Constants.vbLf)
+					return ScannerStatus.Finished;
+				break;
+		}
+		return ScannerStatus.Unmatch;
+	}
+}
 
-    Sub New(ByVal Code As String)
-        Reader = New SourceCodeReader(Code)
-        RulesContainer = New LexiconRules
-        TokenizerRules.LoadLexicalRules(RulesContainer)
-    End Sub
+public class Tokenizer
+{
+	SourceCodeReader Reader;
 
-    Function NextToken() As Token
-        If AscW(Reader.Peek()) = 0 Then Return Nothing
-        Dim Rules = RulesContainer.Keys
-        Dim Matches As New BitArray(Rules.Count, True)
+	LexiconRules RulesContainer;
+	public Tokenizer(string Code)
+	{
+		Reader = new SourceCodeReader(Code);
+		RulesContainer = new LexiconRules();
+		TokenizerRules.LoadLexicalRules(RulesContainer);
+	}
 
-        Dim TokenBuffer As New StringBuilder()
-        Dim Character As Char
-        Dim CandidatePos As Integer
+	public Token NextToken()
+	{
+		if (Strings.AscW(Reader.Peek()) == 0)
+			return null;
+		dynamic Rules = RulesContainer.Keys;
+		BitArray Matches = new BitArray(Rules.Count, true);
 
-        Do
-            CandidatePos = 0
-            Character = Reader.NextChar()
+		StringBuilder TokenBuffer = new StringBuilder();
+		char Character = '\0';
+		int CandidatePos = 0;
 
-            While CandidatePos < Rules.Count
-                If Not Matches(CandidatePos) Then CandidatePos += 1 : Continue While
-                Dim Candidate As LexiconType = Rules(CandidatePos)
-                Select Case RulesContainer(Candidate).Scan(Character, TokenBuffer.Length)
-                    Case ScannerStatus.Continued
-                        Exit Select
-                    Case ScannerStatus.Finished
-                        TokenBuffer.Append(Character)
-                        Return New Token With {.Type = Candidate, .Value = TokenBuffer.ToString}
+		do {
+			CandidatePos = 0;
+			Character = Reader.NextChar();
 
-                    Case ScannerStatus.PreviousFinished
-                        If TokenBuffer.Length > 0 Then
-                            Reader.MovePrev()
-                            Return New Token With {.Type = Candidate, .Value = TokenBuffer.ToString}
-                        Else
-                            Matches.Set(CandidatePos, False)
-                        End If
-                    Case ScannerStatus.Unmatch
-                        Matches.Set(CandidatePos, False)
-                End Select
-                CandidatePos += 1
-            End While
+			while (CandidatePos < Rules.Count) {
+				if (!Matches(CandidatePos)){CandidatePos += 1;continue;}
+				LexiconType Candidate = Rules(CandidatePos);
+				switch (RulesContainer(Candidate).Scan(Character, TokenBuffer.Length)) {
+					case ScannerStatus.Continued:
+						break; // TODO: might not be correct. Was : Exit Select
 
-            'Judge if there's still some candidates are true.
-            For Each IsMatch As Boolean In Matches
-                If IsMatch Then
-                    TokenBuffer.Append(Character)
-                    Continue Do
-                End If
-            Next
+						break;
+					case ScannerStatus.Finished:
+						TokenBuffer.Append(Character);
+						return new Token {
+							Type = Candidate,
+							Value = TokenBuffer.ToString
 
-            Throw New UnexpectedCharacterException(Reader.Position, Character)
-        Loop While Not AscW(Character) = 0
+						};
+					case ScannerStatus.PreviousFinished:
+						if (TokenBuffer.Length > 0) {
+							Reader.MovePrev();
+							return new Token {
+								Type = Candidate,
+								Value = TokenBuffer.ToString
+							};
+						} else {
+							Matches.Set(CandidatePos, false);
+						}
+						break;
+					case ScannerStatus.Unmatch:
+						Matches.Set(CandidatePos, false);
+						break;
+				}
+				CandidatePos += 1;
+			}
 
-        Return Nothing
-    End Function
+			//Judge if there's still some candidates are true.
+			foreach (bool IsMatch in Matches) {
+				if (IsMatch) {
+					TokenBuffer.Append(Character);
+					continue;
+				}
+			}
 
-End Class
+			throw new UnexpectedCharacterException(Reader.Position, Character);
+		} while (!(Strings.AscW(Character) == 0));
 
-Public Class SourceCodeReader
-    Dim BaseString As String
-    Dim Pointer As Integer
+		return null;
+	}
 
-    Public ReadOnly Property Position As Integer
-        Get
-            Return Pointer
-        End Get
-    End Property
+}
 
-    Sub New(ByRef BaseString As String)
-        Me.BaseString = BaseString
-    End Sub
+public class SourceCodeReader
+{
+	string BaseString;
 
-    Public Function Peek() As Char
-        If Pointer < 0 OrElse Pointer >= BaseString.Length Then
-            Return ChrW(0)
-        Else
-            Return BaseString(Pointer)
-        End If
-    End Function
+	int Pointer;
+	public int Position {
+		get { return Pointer; }
+	}
 
-    Public Overloads Function NextChar() As Char
-        Dim result As Char = Peek()
-        MoveNext()
-        Return result
-    End Function
+	public SourceCodeReader(ref string BaseString)
+	{
+		this.BaseString = BaseString;
+	}
 
-    'Reserve
-    Public Overloads Function NextChar(ByVal ch As Char) As Boolean
-        If Peek() = ch Then
-            MoveNext()
-            Return True
-        Else : Return False
-        End If
-    End Function
+	public char Peek()
+	{
+		if (Pointer < 0 || Pointer >= BaseString.Length) {
+			return Strings.ChrW(0);
+		} else {
+			return BaseString(Pointer);
+		}
+	}
 
-    Public Sub MoveNext()
-        Pointer += 1
-    End Sub
+	public char NextChar()
+	{
+		char result = Peek();
+		MoveNext();
+		return result;
+	}
 
-    Public Sub MovePrev()
-        Pointer -= 1
-    End Sub
-End Class
+	//Reserve
+	public bool NextChar(char ch)
+	{
+		if (Peek() == ch) {
+			MoveNext();
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public void MoveNext()
+	{
+		Pointer += 1;
+	}
+
+	public void MovePrev()
+	{
+		Pointer -= 1;
+	}
+}
+
+//=======================================================
+//Service provided by Telerik (www.telerik.com)
+//Conversion powered by NRefactory.
+//Twitter: @telerik
+//Facebook: facebook.com/telerik
+//=======================================================
