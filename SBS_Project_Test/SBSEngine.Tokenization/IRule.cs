6ada9d5111c3c6c3ca0f002f07d927d267f725b6
TerryGeng <gengyanda@gmail.com>
2013-12-28 12:41:53 +0800

    [RE] Add IRule.

---
+++ SBS_Project_Test/SBSEngine.Tokenization/IRule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SBSEngine.Tokenization
+{
+    public interface IRule
+    {
+        ScannerResult Scan(char character);
+        Token Pack(StringBuilder buffer);
+        void Reset();
+    }
+}
