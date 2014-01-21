9a349537f833971de5e4e8c0dc51dc818a3f4162
TerryGeng <gengyanda@gmail.com>
2014-01-21 19:33:26 +0800

    [AD] Finished the basical structure of parser. Added a unfinshed expression parser.

---
+++ SBS_Project_Test/Parsing/IStatment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SBSEngine.Parsing
+{
+    public interface IStatment
+    {
+        void Accept(); // Visitor pattern
+    }
+}
