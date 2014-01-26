f0892719caf063c5bca4718352b6cc3960181fdb
TerryGeng <gengyanda@gmail.com>
2014-01-26 23:03:11 +0800

    [AD] Added ParserCache<T>.

---
+++ SBS_Project_Test/Parsing/PackerCache.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SBSEngine.Tokenization;
+
+namespace SBSEngine.Parsing
+{
+    class PackerCache<T> where T:IPacker,new()
+    {
+        static T instance;
+
+        static public T Instance(Tokenizer tokenizer,Scope scope)
+        {
+            if (instance == null)
+            {
+                instance = new T();
+            }
+
+            instance.Tokenizer = tokenizer;
+            instance.Scope = scope;
+
+            return instance;
+        }
+    }
+}
