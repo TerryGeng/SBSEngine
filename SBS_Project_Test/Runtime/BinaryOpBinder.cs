e5bf6e2c77ae86fbf9566fdf394b467142875932
TerryGeng <gengyanda@gmail.com>
2014-02-01 00:23:38 +0800

    [AD] Added Runtime Binder.

---
+++ SBS_Project_Test/Runtime/BinaryOpBinder.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.CompilerServices;
+using SBSEngine.Runtime;
+
+namespace SBSEngine.Runtime
+{
+    /// <summary>
+    /// This is a binder which provide runtime operations like add, subtract.
+    /// </summary>
+    class BinaryOpBinder : CallSiteBinder
+    {
+        public override T BindDelegate<T>(CallSite<T> site, object[] args)
+        {
+            Type t1 = args[0].GetType();
+            Type t2 = args[1].GetType();
+
+            if (t1 == t2)
+            {
+                if (t1 == typeof(int))
+                {
+                    switch ((SBSOperator)args[3])
+                    {
+                        case SBSOperator.Add:
+                            return AddOperation<T>(args[0],args[1]);
+                    }
+                    
+                }
+            }
+
+            throw new ApplicationException();
+        }
+
+        public T AddOperation<T>(object left, object right)
+        {
+            if (left.GetType() == typeof(int))
+            {
+                return (T)(Object)new Func<CallSite, object, object, object>(IntAdd);
+            }
+
+            return default(T);
+        }
+
+        public object IntAdd(CallSite site, object left, object right)
+        {
+            if (left.GetType() == typeof(int) && right.GetType() == typeof(int))
+            {
+                return (int)left + (int)right;
+            }
+
+            return ((CallSite<Func<CallSite, object, object, object>>)site).Update(site, left, right);
+        }
+    }
+}
